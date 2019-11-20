33eb5c7f48b5a028edc62234aaa078e99d852765
jromero <jonathan.joseromero@gmail.com>
2019-11-20 00:59:16 -0300

    finish test horizontal

--- isMutant.js
+++ isMutant.js
@@ -6,7 +6,8 @@ let isMutant = function (dna) {
     for (let y = 0; y < dnaArray[0].length; y++) {
         for (let x = 0; x < dnaArray.length; x++) {
             let charToCheck = dnaArray[y][x];
-            console.log(testVertical(y, x, charToCheck, dnaArray))
+            //console.log(testVertical(y, x, charToCheck, dnaArray))
+            console.log(testHorizontal(y, x, charToCheck, dnaArray))
         }
     }
 }
@@ -18,6 +19,20 @@ const testVertical = function (indexY, indexX, charToCheck, dnaArray) {
     let result = true;
     for (let x = 1; x < 4; x++) {
         if (dnaArray[indexY + x][indexX] !== charToCheck) {
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
+
+const testHorizontal = function (indexY, indexX, charToCheck, dnaArray) {
+    if (3 - (indexX + 1) < 0) {
+        return false;
+    }
+    let result = true;
+    for (let x = 1; x < 4; x++) {
+        if (dnaArray[indexY][indexX + x] !== charToCheck) {
             result = false;
             break;
         }
